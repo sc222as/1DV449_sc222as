0bba2d8f9f549c64175162efbc1cf7e5bfc43f8c
sc222as <sc222as@student.lnu.se>
2014-01-08 22:19:35 +0100

    Börjar få rätt på det

--- Laboration3/js/lab3.js
+++ Laboration3/js/lab3.js
@@ -1,33 +1,58 @@
 
 function initialize() {
-    
+
         var mapOptions = {
           center: new google.maps.LatLng(60.000, 15.644),
           zoom: 6
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-      }
-      //google.maps.event.addDomListener(window, 'load', initialize);
+                var hemma = new google.maps.LatLng(56.2394, 12.8698);
+              var marker = new google.maps.Marker({
+                  position: hemma,
+                  map: map,
+                  title: "Hello World!"
+              });      
+              marker.setMap(map);
 
-      function getMarkers() {
-          $.get("http://api.sr.se/api/v2/traffic/messages", { history: 100 }, function (data) {
-              $(data).find("message").each(function () {
+ 
+            
 
-                  $("body").append($(this).find('description').text() + "hej");
-              });
+              $.ajax({
+                  url: 'http://api.sr.se/api/v2/traffic/messages?format=json&pagination=false&indent=true&size=100',
+                  dataType: 'json',
+                  success: function (data) {
+                      alert("so far so good1")
+                      var text = '';
+                      var messages = data.messages;
+                      var len = messages.length;
+                      for (var i = 0; i < len; i++) {
+
+                          var hemma = new google.maps.LatLng(messages[i].latitude, messages[i].longitude);
+                          var marker = new google.maps.Marker({
+                              position: hemma,
+                              map: map,
+                              title: messages[i].title
+                          });
+                          marker.setMap(map);
 
 
-          });
+                          text += '<p>' + +'<br/ >' + +'<br/ >' + +'</p>'
+
+                      }
+                      $('body').html(text);
+
+                  }
+
+              }); 
 
 
-      };
 
 
-      //var hemma = new google.maps.LatLng(56.2394, 12.8698);
-      //var marker = new google.maps.Marker({
-      //    position: hemma,
-      //    map: map,
-      //    title: "Hello World!"
-      //});      
-      //marker.setMap(map);
+}; 
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
